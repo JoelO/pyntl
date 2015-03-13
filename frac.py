d7827819378bc5198969025737738de0cfe43aff
Spike Harris <spike@spike.org>
2015-03-13 03:53:57 -0400

    Added my custom fraction class. Will put up continued fraction stuff when it's cleaner.

---
+++ frac.py
@@ -0,0 +1,118 @@
+class Fraction:
+    '''
+    A class to handle operations on rational numbers.
+
+    Note: This class does no sanity or type checking.
+    You CAN divide by zero, if that's what you're into.
+    '''
+
+    #################
+    ## Constructor ##
+    #################
+
+    def __init__(self, numerator = 0, denominator = 1):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    #####################
+    ## Type Conversion ##
+    #####################
+
+    def __str__(self):
+        return str(self.numerator) + " / " + str(self.denominator)
+
+    def __int__(self):
+        return self.numerator // self.denominator
+
+    def __float__(self):
+        return float(self.numerator) / self.denominator
+
+    def __bool__(self):
+        return self.numerator
+
+    def __nonzero__(self):
+        # Needed for Python 2
+        return self.__bool__()
+
+    ##########################
+    ## Arithmetic operators ##
+    ##########################
+
+    def __neg__(self):
+        return Fraction(-self.numerator, self.denominator)
+
+    def __add__(self, other):
+        newNumerator = self.numerator * other.denominator + \
+                        self.denominator * other.numerator
+        newDenominator = self.denominator * other.denominator
+        return Fraction(newNumerator, newDenominator)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    # FIXME: This doesn't allow division by integers.
+    def __div__(self, other):
+        return self * other.reciprocal()
+
+    def __rdiv__(self, other):
+        return other * self.reciprocal()
+
+    def __mul__(self, other):
+        newNumerator = self.numerator * other.numerator
+        newDenominator = self.denominator * other.denominator
+        return Fraction(newNumerator, newDenominator)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __pow__(self, power):
+        newNumerator = self.numerator ** power
+        newDenominator = self.denominator ** power
+        return Fraction(newNumerator, newDenominator)
+
+    def reciprocal(self):
+        return Fraction(self.denominator, self.numerator)
+
+    def reduce(self):
+        g = _gcd(self.numerator, self.denominator)
+        if g != 0:
+            self.numerator /= g
+            self.denominator /= g
+        return self
+
+    ##########################
+    ## Comparison Operators ##
+    ##########################
+
+    def __eq__(self, other):
+        return  self.numerator * other.denominator == \
+                self.denominator * other.numerator
+
+    def __lt__(self, other):
+        return  self.numerator * other.denominator < \
+                self.denominator * other.numerator
+
+    def __gt__(self, other):
+        return other < self
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __ge__(self, other):
+        return not (self < other)
+
+    def __le__(self, other):
+        return not (self > other)
+
+# TODO: Factor this out.
+def _gcd(a, b):
+    while a != 0:
+        (a, b) = (b % a, a)
+    return b
+
